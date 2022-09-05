e5a5e10842d9f39787ee740de05f82182a8e27a1
Ronak_Suthar <ronakronu02@gmail.com>
2022-09-05 22:41:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,57 @@
+console.log("welcome to Play XO")
+let music = new Audio("music1.ogg")
+let audioturn = new Audio("music1.ogg")
+let bgmusic= new Audio("music2.mp3")
+
+let turn ="X"
+let gaemover=false;
+const changeTurn =()=>{
+    return turn==="X"?"O":"X"
+}
+
+//chck wi
+const checkwin = () =>
+{
+    let boxtext=document.getElementsByClassName('boxtext');
+    let wins= [
+        [0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]
+    ]
+    wins.forEach(e=>{
+        if((boxtext[e[0]].innerText=== boxtext[e[1]].innerText) &&(boxtext[e[2]].innerText=== boxtext[e[1]].innerText) && (boxtext[e[0]].innerText!== ""))
+        {
+            document.querySelector('.info').innerText=boxtext[e[0]].innerText+ " WON ";
+            gaemover=true;
+        }
+    })
+}
+
+//main
+bgmusic.play()
+let boxes=document.getElementsByClassName("box");
+Array.from(boxes).forEach(element=>{
+    let boxtext=element.querySelector('.boxtext');
+    element.addEventListener('click',()=>{
+        if(boxtext.innerText==='')
+        {
+            boxtext.innerText=turn;
+            turn=changeTurn();
+            audioturn.play();
+            checkwin();
+            if(!gaemover){
+
+                document.getElementsByClassName("info")[0].innerText="Turn for " + turn;
+            }
+        
+        }
+    })
+})
+
+reset.addEventListener('click',()=>{
+    let boxtexts=document.querySelectorAll('.boxtext');
+    Array.from(boxtexts).forEach(element=>{
+        element.innerText="";
+    });
+    turn="X";
+gaemover=false
+ document.getElementsByClassName("info")[0].innerText="Turn for " + turn;
+})
